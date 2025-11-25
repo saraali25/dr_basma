462d795a4d5ff2b8c145d11da1fc7e4bc70a5e34
dfouad <eng.doaa.fouad@gmail.com>
2025-11-25 08:36:06 +0200

    Add new CORS origin to settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -197,7 +197,7 @@ SIMPLE_JWT = {
 # CORS settings
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:5173,http://127.0.0.1:5173,http://127.0.0.1:8080,http://localhost:8080,https://your-frontend.lovable.app,https://drbasma.vercel.app/,https://basmakamal.com,https://www.basmakamal.com,https://api.basmakamal.com',  
+    default='http://localhost:5173,http://127.0.0.1:5173,http://127.0.0.1:8080,http://localhost:8080,https://your-frontend.lovable.app,https://drbasma.vercel.app/,https://basmakamal.com,https://www.basmakamal.com,https://api.basmakamal.com,https://www.basmakamal.com',  
 ).split(',')
 
 CORS_ALLOW_ALL_ORIGINS = True
