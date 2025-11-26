91389e55ade183ed492aa1124037f84444b67c2c
amiraibrahim86 <eng.amiraibrahim@gmail.com>
2025-11-26 23:27:36 +0200

    Refactor isFree logic in CourseCard component

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -47,8 +47,8 @@ const CourseCard = ({
   price,
   is_free,
 }: CourseCardProps) => {
-  //const isFree = is_free || price === null || price === 0;
-  const isFree = is_free ;
+  const isFree = is_free || price === null || price === 0;
+  //const isFree = is_free ;
   return (
     <div className="group bg-card rounded-lg overflow-hidden shadow-md hover:shadow-xl transition-all duration-300 border border-border">
       {/* 🖼 Course image area */}
